22b17f4df5c5bae4e21bb54c5aed63a11c6c2c62
xietian <2454218998@qq.com>
2024-03-19 23:01:57 +0800

    in home add a litter file

--- src/components/VantPicker.vue
+++ src/components/VantPicker.vue
@@ -1,8 +1,15 @@
 <script setup>
+const columns = reactive([
+  ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+  ['上海', '江苏', '浙江', '安徽', '福建'],
+  ['武汉', '北京', '天津', '河北', '山西'],
+  ['重庆', '四川', '贵州', '云南', '西藏'],
+  ['辽宁', '吉林', '黑龙江', '内蒙古', '广西']
+  )
 </script>
 
 <template>
-
+  <van-picker title="标题" :columns="columns" />
 </template>
 
 <style></style>
