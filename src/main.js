05d18678153702a1ba523f8928206db30addc20e
xietian <2454218998@qq.com>
2024-03-28 20:58:07 +0800

    in home add a litter file

--- src/main.js
+++ src/main.js
@@ -14,12 +14,13 @@ console.log(import.meta.env);
 // 但是我们可以通过import.meta.env来获取环境变量 反正经我测试没办法去修改process.env.ENV_MODE的值
 
 const app = createApp(App) 
+console.log(app);
 // 创建一个pinia应用实例 可以被app应用所使用 因为其内部含有一个install方法
 const pinia = createPinia()
 // 打印这个pinia应用实例可以看到 我们定义的多个store都存在于其身上 官网说使用createPinia 并使用use后才能提供devtool的支持 并且如果不这么做 直接使用defineStore是会报错的 可以认为不使用这个 就无法使用pinia
 
 // pinia可以使用use属性来使用插件  https://blog.csdn.net/qq_33635385/article/details/125339178 createPinia源码
-console.log(pinia);
+
 app.use(router)
 app.use(pinia)
 app.mount('#app')
