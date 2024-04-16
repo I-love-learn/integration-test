5579ae043dd9c9b5bb5114b96fa220a8a416869e
xietian <2454218998@qq.com>
2024-04-16 23:13:34 +0800

    in home add a litter file

--- src/auto-import.d.ts
+++ src/auto-import.d.ts
@@ -98,6 +98,8 @@ declare module 'vue' {
   interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
+    readonly ElMessage: UnwrapRef<typeof import('element-plus/es')['ElMessage']>
+    readonly ElMessageBox: UnwrapRef<typeof import('element-plus/es')['ElMessageBox']>
     readonly acceptHMRUpdate: UnwrapRef<typeof import('pinia')['acceptHMRUpdate']>
     readonly cloneDeep: UnwrapRef<typeof import('lodash')['cloneDeep']>
     readonly computed: UnwrapRef<typeof import('vue')['computed']>
@@ -181,6 +183,8 @@ declare module '@vue/runtime-core' {
   interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
+    readonly ElMessage: UnwrapRef<typeof import('element-plus/es')['ElMessage']>
+    readonly ElMessageBox: UnwrapRef<typeof import('element-plus/es')['ElMessageBox']>
     readonly acceptHMRUpdate: UnwrapRef<typeof import('pinia')['acceptHMRUpdate']>
     readonly cloneDeep: UnwrapRef<typeof import('lodash')['cloneDeep']>
     readonly computed: UnwrapRef<typeof import('vue')['computed']>
