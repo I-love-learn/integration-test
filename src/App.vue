1b6249ce66b353473e2840fcde7d7c17d9ffce7c
xietian <2454218998@qq.com>
2024-07-14 12:08:16 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
-  import "@/assets/css/base.css"
+import "@/assets/css/base.css"
+import h from "@/components/h.vue"
 </script>
 
 <template>
+  <h />
   <router-view></router-view>
 </template>
 
 <style lang="scss">
-  #app {
-    // color: $color;
-    height: 100vh;
-  }
+#app {
+  // color: $color;
+  height: 100vh;
+}
 </style>
---
+++ src/components/h.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { h } from "vue"
+// 不知道为什么eslint格式化会自动在函数前加; 感觉这个;加了没有任何意义不加也不会报错 但是这个代码是没办法使用的 因为箭头函数也要赋值的
+// setup语法糖没法 return 一个render
+;(() => {
+  h("div", { id: "map" })
+  console.log(123)
+  return 123
+})()
+</script>
