4cb6af6f56ee0fbc4157193d5eb561dc917f6e9c
xietian <2454218998@qq.com>
2024-08-15 23:17:07 +0800

    123

--- src/views/TestFive.vue
+++ src/views/TestFive.vue
@@ -58,6 +58,60 @@ onMounted(() => {
 })
 
 const input3 = ref()
+
+const objectSpanMethod = ({ row, column, rowIndex, columnIndex }) => {
+  if (columnIndex === 0) {
+    if (rowIndex % 2 === 0) {
+      return {
+        rowspan: 2,
+        colspan: 1
+      }
+    } else {
+      return {
+        rowspan: 0,
+        colspan: 0
+      }
+    }
+  }
+}
+
+const tableData = [
+  {
+    id: "12987122",
+    name: "Tom",
+    amount1: "234",
+    amount2: "3.2",
+    amount3: 10
+  },
+  {
+    id: "12987123",
+    name: "Tom",
+    amount1: "165",
+    amount2: "4.43",
+    amount3: 12
+  },
+  {
+    id: "12987124",
+    name: "Tom",
+    amount1: "324",
+    amount2: "1.9",
+    amount3: 9
+  },
+  {
+    id: "12987125",
+    name: "Tom",
+    amount1: "621",
+    amount2: "2.2",
+    amount3: 17
+  },
+  {
+    id: "12987126",
+    name: "Tom",
+    amount1: "539",
+    amount2: "4.1",
+    amount3: 15
+  }
+]
 </script>
 
 <template>
@@ -69,6 +123,22 @@ const input3 = ref()
         <t2-input v-model="value" @input="testInput1" ref="input3" />
         <el-input v-model="value" @input="testInput1" ref="input" />
         <input type="text" v-model="value" @input="testInput1" />
+      </fieldset>
+
+      <fieldset>
+        <legend>table 合并行后 hover样式自定义</legend>
+        <el-table
+          :data="tableData"
+          :span-method="objectSpanMethod"
+          border
+          style="width: 100%; margin-top: 20px"
+        >
+          <el-table-column prop="id" label="ID" width="180" />
+          <el-table-column prop="name" label="Name" />
+          <el-table-column prop="amount1" label="Amount 1" />
+          <el-table-column prop="amount2" label="Amount 2" />
+          <el-table-column prop="amount3" label="Amount 3" />
+        </el-table>
       </fieldset>
     </form>
   </div>
