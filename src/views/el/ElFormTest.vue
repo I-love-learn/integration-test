6c961ca77fcbf78d32af94125ae81a5ea5e5f547
xt <2454218998@qq.com>
2024-10-30 17:08:40 +0800

    改bug

--- src/views/el/ElFormTest.vue
+++ src/views/el/ElFormTest.vue
@@ -25,6 +25,53 @@ const rules = reactive({
       //
       min: 1
     }
+  ],
+  eat: [
+    {
+      required: true,
+      message: "请输入吃饭",
+      trigger: ["blur"],
+      //
+      min: 1
+    }
+  ],
+  drink: [
+    {
+      required: true,
+      message: "请输入饮料",
+      trigger: ["change"]
+    }
+  ],
+  fruit: [
+    {
+      required: true,
+      message: "请输入水果",
+      trigger: ["change"]
+    }
+  ],
+  isPay: [
+    {
+      required: true,
+      message: "请选择是否支付",
+      trigger: ["change"]
+    }
+  ],
+  pay: [
+    {
+      required: true,
+      min: 10,
+      max: 999,
+      trigger: ["blur"],
+      message: "请输入正确金额"
+    }
+  ],
+  time: [
+    {
+      required: true,
+      type: "date",
+      trigger: ["change", "blur"],
+      message: "请选择时间"
+    }
   ]
 })
 
@@ -34,6 +81,22 @@ const open = ref(false)
 const formRef = ref()
 function submit() {
   formRef.value.validate((valid) => {
+    if (valid) {
+      ElMessage.success("验证成功")
+    } else {
+      ElMessage.error("验证失败")
+    }
+  })
+}
+
+const formT = ref({
+  detail: ""
+})
+
+const open2 = ref(false)
+const dg = ref()
+function sb() {
+  dg.value.validate((valid) => {
     if (valid) {
       ElMessage.success("验证成功")
     } else {
@@ -93,7 +156,71 @@ function submit() {
       >渲染form</el-button
     >
     <el-button @click="open = true">渲染form</el-button>
+    <el-button @click="open2 = true">渲染大form</el-button>
   </div>
+
+  <el-dialog v-model="open2">
+    <el-form :model="formT" :rules="rules" ref="dg">
+      <el-form-item label="吃饭" prop="eat">
+        <el-input v-model="formT.eat" />
+      </el-form-item>
+
+      <el-form-item label="饮料" prop="drink">
+        <!-- radio-group的v-model 一上来必须有初始值 否则所有的el-radio会选中 -->
+        <el-radio-group v-model="formT.drink">
+          <el-radio
+            v-for="(item, index) in [
+              { name: 'water', id: 1 },
+              { name: 'coffee', id: 2 }
+            ]"
+            :key="index"
+            :value="item.id"
+            >{{ item.name }}</el-radio
+          >
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="水果" prop="fruit">
+        <el-select v-model="formT.fruit">
+          <el-option
+            v-for="item in ['apple', 'orange']"
+            :key="item"
+            :label="item"
+            :value="item"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="是否自费" prop="isPay">
+        <el-switch v-model="formT.isPay"> </el-switch>
+      </el-form-item>
+
+      <el-form-item label="花多少钱" prop="pay">
+        <el-input v-model="formT.pay"> </el-input>
+      </el-form-item>
+
+      <el-form-item label="活动星级" prop="starRating">
+        <el-rate v-model="formT.starRating" />
+      </el-form-item>
+
+      <el-form-item label="活动感受" prop="detail">
+        <el-input type="textarea" v-model="formT.detail"> </el-input>
+      </el-form-item>
+
+      <el-form-item label="活动时间" prop="time">
+        <el-date-picker
+          v-model="formT.time"
+          type="datetime"
+          placeholder="选择日期时间"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <el-button @click="open2 = false">取消</el-button>
+      <el-button @click="sb">提交</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <style lang="scss" scoped></style>
